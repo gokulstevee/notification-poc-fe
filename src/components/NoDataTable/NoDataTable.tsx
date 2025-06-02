eefa2831fa2c398a575e33fa5e64be54a08c11ae
gokulan-gag <gokulan@gridsandguides.com>
2025-06-02 21:39:17 +0530

    changes in no data table

--- src/components/NoDataTable/NoDataTable.tsx
+++ src/components/NoDataTable/NoDataTable.tsx
@@ -1,7 +1,7 @@
 const NoDataTable = () => {
   return (
     <div className="text-[#6a6a6a9b] text-[1.1rem] text-center py-[5rem]">
-      No Records Available
+      No Posts Available
     </div>
   );
 };
